e7c42550751efbb7904671e9478ea66305b983d9
Nutchapon Hanouypornlert <nutcha.h@Nutchapons-MacBook-Pro.local>
2022-11-07 21:37:22 +0700

    [FE-#5] - setup tailwind and styled component

--- frontend/app/HomePage.tsx
+++ frontend/app/HomePage.tsx
@@ -1,11 +1,24 @@
 'use client';
 
-import { useContext } from 'react';
+import { ReactNode, useContext } from 'react';
+import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
 
 import { AuthContext } from './authContext';
 
 export default function HomePage() {
   const auth = useContext(AuthContext);
 
-  return <div>Hello Home</div>;
+  return (
+    <>
+      <Container>
+        <div className="text-3xl font-bold underline">Hello Home</div>
+      </Container>
+      <div>heasads2</div>
+    </>
+  );
 }
+
+const Container = styled.div`
+  min-height: 100vh;
+`;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,3 @@
-import { useContext } from 'react';
-
 import HomePage from './HomePage';
 
 export default function Page() {
